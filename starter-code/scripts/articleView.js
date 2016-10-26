ccb698d235c549bdddb3b053b41add519e07be96
Ted Callahan <CCallahanIV@gmail.com>
2016-10-26 15:27:27 -0700

    lecture notes

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -25,9 +25,12 @@ articleView.handleAuthorFilter = function() {
             that was aselected. Hint: use an attribute selector to find
             those articles that match the value, and then fade them in.
         */
+      $('article').hide();
+      $('article a:contains($(this.val()))').fadeIn(400);
     } else {
     /* Otherwise, we should:
         1. Show all the articles except the template */
+      $('article').not('.template').fadeIn(400);
     }
     $('#category-filter').val('');
   });
